e46d4f1c303e797fed02116aa7e27658287379d4
haashirmemon <haashirmemon6@gmail.com>
2024-05-15 21:49:04 -0700

    file updated

---
+++ assigment image gallery/app.js
@@ -0,0 +1,80 @@
+var mainimage = document.getElementById("main")
+var img1 = document.getElementById("div1")
+var img2 = document.getElementById("div2")
+var img3 = document.getElementById("div3")
+var img4 = document.getElementById("div4")
+var img5 = document.getElementById("div5")
+
+img1.addEventListener('mouseover',function(){
+    mainimage.src = this.src
+    // this.src="https://media3.giphy.com/media/xTiTnFp4wEtqM7eUJW/200w.webp?cid=ecf05e47fhfltxg3pl806atpi0efquxvx3szz8fwcn4y5uw8&ep=v1_gifs_related&rid=200w.webp&ct=g"
+})
+img2.addEventListener('mouseover',function(){
+  mainimage.src = this.src
+//   this.src="https://media3.giphy.com/media/xTiTnFp4wEtqM7eUJW/200w.webp?cid=ecf05e47fhfltxg3pl806atpi0efquxvx3szz8fwcn4y5uw8&ep=v1_gifs_related&rid=200w.webp&ct=g"
+})
+img3.addEventListener('mouseover',function(){
+  mainimage.src = this.src;
+  //   this.src="https://media3.giphy.com/media/xTiTnFp4wEtqM7eUJW/200w.webp?cid=ecf05e47fhfltxg3pl806atpi0efquxvx3szz8fwcn4y5uw8&ep=v1_gifs_related&rid=200w.webp&ct=g"
+})
+img4.addEventListener('mouseover',function(){
+ mainimage.src = this.src;
+ //   this.src="https://media3.giphy.com/media/xTiTnFp4wEtqM7eUJW/200w.webp?cid=ecf05e47fhfltxg3pl806atpi0efquxvx3szz8fwcn4y5uw8&ep=v1_gifs_related&rid=200w.webp&ct=g"
+ 
+ })
+ img5.addEventListener('mouseover',function(){
+   mainimage.src= this.src; 
+   //   this.src="https://media3.giphy.com/media/xTiTnFp4wEtqM7eUJW/200w.webp?cid=ecf05e47fhfltxg3pl806atpi0efquxvx3szz8fwcn4y5uw8&ep=v1_gifs_related&rid=200w.webp&ct=g"
+ })
+
+//  img1.addEventListener('mouseout',function(){
+//   this.src = mainimage
+    
+// })
+// img2.addEventListener('mouseout',function(){
+ 
+//   this.src = mainimage
+    
+//   })
+//  img3.addEventListener('mouseout',function(){
+//   this.src = mainimage
+  
+// })
+// img4.addEventListener('mouseout',function(){
+//   this.src = mainimage
+
+//  })
+//  img5.addEventListener('mouseout',function(){
+//   this.src = mainimage
+//  })
+
+
+//  console.log(img1);
+//  console.log(img2);
+//  console.log(img3);
+//  console.log(img4);
+//  console.log(img5);
+
+
+
+img1.addEventListener('mouseout',function(){
+  mainimage.src = this.src
+  // this.src="https://media3.giphy.com/media/xTiTnFp4wEtqM7eUJW/200w.webp?cid=ecf05e47fhfltxg3pl806atpi0efquxvx3szz8fwcn4y5uw8&ep=v1_gifs_related&rid=200w.webp&ct=g"
+})
+img2.addEventListener('mouseout',function(){
+mainimage.src = this.src
+//   this.src="https://media3.giphy.com/media/xTiTnFp4wEtqM7eUJW/200w.webp?cid=ecf05e47fhfltxg3pl806atpi0efquxvx3szz8fwcn4y5uw8&ep=v1_gifs_related&rid=200w.webp&ct=g"
+})
+img3.addEventListener('mouseout',function(){
+mainimage.src = this.src;
+//   this.src="https://media3.giphy.com/media/xTiTnFp4wEtqM7eUJW/200w.webp?cid=ecf05e47fhfltxg3pl806atpi0efquxvx3szz8fwcn4y5uw8&ep=v1_gifs_related&rid=200w.webp&ct=g"
+})
+img4.addEventListener('mouseout',function(){
+mainimage.src= './download (1).jpeg'
+//   this.src="https://media3.giphy.com/media/xTiTnFp4wEtqM7eUJW/200w.webp?cid=ecf05e47fhfltxg3pl806atpi0efquxvx3szz8fwcn4y5uw8&ep=v1_gifs_related&rid=200w.webp&ct=g"
+
+})
+img5.addEventListener('mouseout',function(){
+ mainimage.src= this.src; 
+ //   this.src="https://media3.giphy.com/media/xTiTnFp4wEtqM7eUJW/200w.webp?cid=ecf05e47fhfltxg3pl806atpi0efquxvx3szz8fwcn4y5uw8&ep=v1_gifs_related&rid=200w.webp&ct=g"
+})
